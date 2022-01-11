635c63230fa9678b293891c8725e9fe2e9f3eec7
chryzxc <chriztiann.villablanca@gmal.com>
2022-01-11 20:11:44 +0800

    commit

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react'
-import firebase from "../others/Firebase"
+import db from "../others/Firebase"
 import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import {ref, set, push} from "firebase/database"
 toast.configure()
 
  const Forms = () => {
@@ -9,18 +10,34 @@ toast.configure()
     const [taskName,setTaskName] = useState("");
 
     const createTodo = (e) => {
-        toast("ok");
         e.preventDefault();
-      
 /*
-        const todoRef = firebase.database().ref("Users/chan/Todo");
+        setDoc(doc(db, "cities", "LA"), {
+            name: "Los Angeles",
+            state: "CA",
+            country: "USA"
+          });
+*/
+        const todoRef = db.ref("Users/chan/Todo");  
+        const todo ={
+            taskName,
+            dateCreated: new Date(),
+            completed: false
+        }
+
+        set(ref(db, 'users/' + "test"), todo);
+       
+/*
+        const todoRef = db.ref("Users/chan/Todo");
         const todo ={
             taskName,
             dateCreated: new Date(),
             completed: false
         }
         todoRef.push(todo);
-        */
+    */
+        toast("ok");
+ 
 
     };
     const handleChange = (e) => {
@@ -28,17 +45,17 @@ toast.configure()
     };
 
     return (
-       <form>
-           <input onSubmit={createTodo}
+       <form onSubmit={createTodo}>
+           <input 
            type="text"
-           placeholder="Enter task name"
+           placeholder="Enter task name..."
            className="task-input"
            value={taskName}
            required
            onChange={handleChange}>
            </input>
 
-           <button className="button-add-todo" type="submit">
+           <button className="button-add-todo" type='submit'>
                Add
            </button>
        </form>
